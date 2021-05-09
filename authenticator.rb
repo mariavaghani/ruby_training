8a48ff061db1d3bbd6b642f95d3459e9b1ddcd5f
mariavaghani <maria.vaghani@gmail.com>
2021-05-09 09:37:59 -0400

    refactored to remove unnecesary conditionals

--- authenticator.rb
+++ authenticator.rb
@@ -9,30 +9,30 @@ users = [
 ]
 
 puts "Welcome to the authenticator"
-30.times { print "-"}
+60.times { print "-"}
 puts
 puts "This program will take input from the user and compare password"
 puts "If password is correct, you will get back the user object"
 
-ask_again = true
 attempt = 4
 key_given = false
-while ask_again && attempt > 0 && !key_given
+
+while attempt > 0 && !key_given
   print "Username: "
   name_ask = gets.chomp
   user = users.select { |hash| hash[:username] == name_ask}
   if user.empty?
-    puts "Incorrect username. Please try again"
-    next
+    puts "Incorrect username. Please try again \nPress n to quit or any other key to continue"
+    br = gets.chomp.downcase
+    br == 'n' ? (break) : (next)
   end
-  while (attempt >= 0) && !key_given
+  while (attempt >= 0)
   print "Password: "
   pass_ask = gets.chomp
   if user[0][:password] == pass_ask
     puts "Welcome to the system"
-    ask_again = false
     key_given = true
-    next
+    break
   else
     puts "Incorrect credentials. Please, try again. You have #{attempt} attempt(s) remaining"
     attempt -= 1
