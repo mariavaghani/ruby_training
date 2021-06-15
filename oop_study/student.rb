0eb33d09812e33cdc40921803de1b74c25a7e47a
mariavaghani <maria.vaghani@gmail.com>
2021-06-14 23:43:43 -0400

    added main and crud module

--- oop_study/student.rb
+++ oop_study/student.rb
@@ -1,14 +1,30 @@
+require_relative 'crud'
+
 class Student
-  def initialize(first_name, last_name)
-    @first_name = first_name
-    @last_name = last_name
-  end
+
+  include Crud
+
+  attr_accessor :first_name, :last_name, :email
+  attr_reader :username, :password
   @email
   @Username
   @password
 
+
+  def initialize(first_name, last_name, username, email, password)
+    @first_name = first_name
+    @last_name = last_name
+    @username = username
+    @email = email
+    @password = password
+  end
+
   def to_s
-    "First name: #{@first_name} \nLast name: #{@last_name}"
+    "\tFirst name: #{@first_name}\n\tLast name: #{@last_name}\n\tUsername: #{@username}\n\tEmail: #{@email}"
+  end
+
+  def set_username=(username_val)
+    @username = username_val
   end
 
   def puts_smt
@@ -16,7 +32,7 @@ class Student
   end
 end
 
-maria = Student.new('Maria', 'Vaghani')
-puts maria
-maria.puts_smt
-# maria.first_name = "Maria"
+maria = Student.new('Maria', 'Degtyareva', "MariaVagh", 'mariavagh@gmail.com', 'password1')
+
+hashed_password = maria.create_hash_digest(maria.password)
+puts hashed_password
